5cd79f1a091a377efec19f97e1e5cab0b126e1dd
idoye <idoye@campus.technion.ac.il>
2020-05-10 10:38:47 +0300

    adding logger to optimal solver

--- Learning_to_Route/RL-ECMP/flow_routing/find_optimal_load_balancing.py
+++ Learning_to_Route/RL-ECMP/flow_routing/find_optimal_load_balancing.py
@@ -2,25 +2,30 @@ import pulp as pl
 from consts import EdgeConsts
 from ecmp_network import ECMPNetwork, nx
 from collections import defaultdict
+import logging
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
 
-def get_optimal_load_balancing(net: ECMPNetwork, traffic_demand, verbos=0):
+def get_optimal_load_balancing(net: ECMPNetwork, traffic_demand):
     lp_problem = pl.LpProblem('Load_Balancing', pl.LpMinimize)
     vars_dict = dict()  # dictionary to store all variable problem
 
     # the object variable and function.
+    logging.info("Creating linear programing problem")
     r = pl.LpVariable("r", lowBound=0, upBound=1)
     vars_dict["r"] = r
     lp_problem += r
 
     vars_per_edge = defaultdict(list)  # dictionary to save all variable related to edge.
-
+    logging.info("Handling all flows")
     for nodes_pair in net.get_all_pairs():
         src, dst = nodes_pair
         src_dest_flow = traffic_demand[src][dst]
         if src_dest_flow > 0:
+            logging.debug("Handle flow form {} to {}".format(src, dst))
             vars_per_path = list()
             for path in net.all_simple_paths(source=src, target=dst):
+                logging.debug("Handle the path {}".format(str(path)))
                 var_name = "x_" + '->'.join(str(i) for i in path)
                 var: pl.LpVariable = pl.LpVariable(var_name, lowBound=0)
                 vars_dict[var_name] = var  # adding variable to all vars dict
@@ -28,6 +33,7 @@ def get_optimal_load_balancing(net: ECMPNetwork, traffic_demand, verbos=0):
 
                 for edges_in_path in map(nx.utils.pairwise, [path]):
                     for edge in list(edges_in_path):
+                        logging.debug("Handle edge {} in path {}".format(str(edge), str(path)))
                         if edge[0] > edge[1]:
                             edge = (edge[1], edge[0])
                         vars_per_edge[edge].append(var)
@@ -38,16 +44,15 @@ def get_optimal_load_balancing(net: ECMPNetwork, traffic_demand, verbos=0):
         lp_problem += pl.lpSum(var_list) <= net.get_edge_key(edge, EdgeConsts.CAPACITY_STR) * r
 
     status = lp_problem.solve()
-    if verbos > 0:
-        print(lp_problem)
-        print("Status: {}".format(pl.LpStatus[status]))
-        for var_name, var in vars_dict.items():
-            print("Value of variable: {} is: {}".format(var_name, pl.value(var)))
+    logging.debug(lp_problem)
+    logging.info("Status: {}".format(pl.LpStatus[status]))
+    for var_name, var in vars_dict.items():
+        logging.debug("Value of variable: {} is: {}".format(var_name, pl.value(var)))
 
     return pl.value(r)
 
 
-"""
+
 def get_base_graph():
     # init a triangle if we don't get a network graph
     g = nx.Graph()
@@ -65,5 +70,5 @@ def get_flows_matrix():
 
 ecmpNetwork = ECMPNetwork(get_base_graph())
 
-get_optimal_load_balancing(ecmpNetwork, get_flows_matrix(), verbos=1)
-"""
+get_optimal_load_balancing(ecmpNetwork, get_flows_matrix())
+
