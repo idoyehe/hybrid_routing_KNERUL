4bcc22ba2175bdb295da58ecf901442aa413a25c
idoye <idoye@campus.technion.ac.il>
2020-05-12 19:50:53 +0300

    fix bugs in environment

--- Learning_to_Route/RL-ECMP/flow_routing/find_optimal_load_balancing.py
+++ Learning_to_Route/RL-ECMP/flow_routing/find_optimal_load_balancing.py
@@ -3,6 +3,7 @@ from consts import EdgeConsts
 from ecmp_network import ECMPNetwork, nx
 from collections import defaultdict
 from logger import logger
+import numpy as np
 
 
 def get_optimal_load_balancing(net: ECMPNetwork, traffic_demand):
@@ -35,12 +36,12 @@ def get_optimal_load_balancing(net: ECMPNetwork, traffic_demand):
                         logger.debug("Handle edge {} in path {}".format(str(edge), str(path)))
                         if edge[0] > edge[1]:
                             edge = (edge[1], edge[0])
-                        vars_per_edge[edge].append(var)
+                        vars_per_edge[edge].append((var, nodes_pair))
 
             lp_problem += pl.lpSum(vars_per_path) == src_dest_flow
 
     for edge, var_list in vars_per_edge.items():
-        lp_problem += pl.lpSum(var_list) <= net.get_edge_key(edge, EdgeConsts.CAPACITY_STR) * r
+        lp_problem += pl.lpSum([elem[0] for elem in var_list]) <= net.get_edge_key(edge, EdgeConsts.CAPACITY_STR) * r
 
     status = lp_problem.solve()
     logger.debug(lp_problem)
@@ -48,26 +49,31 @@ def get_optimal_load_balancing(net: ECMPNetwork, traffic_demand):
     for var_name, var in vars_dict.items():
         logger.debug("Value of variable: {} is: {}".format(var_name, pl.value(var)))
 
-    return pl.value(r)
+    per_edge_flow_fraction = dict()
+    for edge, var_list in vars_per_edge.items():
+        edge_per_demend = np.zeros((net.get_num_nodes, net.get_num_nodes))
+        for var, (src, dst) in var_list:
+            edge_per_demend[src][dst] += var.value() / traffic_demand[src][dst]
+        per_edge_flow_fraction[edge] = edge_per_demend
+
+    return pl.value(r), per_edge_flow_fraction
 
 
-#
-# def get_base_graph():
-#     # init a triangle if we don't get a network graph
-#     g = nx.Graph()
-#     g.add_nodes_from([0, 1, 2])
-#     g.add_edges_from([(0, 1, {EdgeConsts.WEIGHT_STR: 1, EdgeConsts.CAPACITY_STR: 10}),
-#                       (0, 2, {EdgeConsts.WEIGHT_STR: 1, EdgeConsts.CAPACITY_STR: 10}),
-#                       (1, 2, {EdgeConsts.WEIGHT_STR: 1, EdgeConsts.CAPACITY_STR: 15})])
-#
-#     return g
-#
-#
-# def get_flows_matrix():
-#     return [[0, 5, 10], [0, 0, 7], [0, 0, 0]]
-#
-#
-# ecmpNetwork = ECMPNetwork(get_base_graph())
-#
-# get_optimal_load_balancing(ecmpNetwork, get_flows_matrix())
+def get_base_graph():
+    # init a triangle if we don't get a network graph
+    g = nx.Graph()
+    g.add_nodes_from([0, 1, 2])
+    g.add_edges_from([(0, 1, {EdgeConsts.WEIGHT_STR: 1, EdgeConsts.CAPACITY_STR: 10}),
+                      (0, 2, {EdgeConsts.WEIGHT_STR: 1, EdgeConsts.CAPACITY_STR: 10}),
+                      (1, 2, {EdgeConsts.WEIGHT_STR: 1, EdgeConsts.CAPACITY_STR: 15})])
 
+    return g
+
+
+def get_flows_matrix():
+    return [[0, 5, 10], [0, 0, 7], [0, 0, 0]]
+
+
+ecmpNetwork = ECMPNetwork(get_base_graph())
+
+get_optimal_load_balancing(ecmpNetwork, get_flows_matrix())
