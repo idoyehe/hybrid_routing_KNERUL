5930906480183defe98be48948738f9a2451c06c
IdoYe <idoye@campus.technion.ac.il>
2020-05-14 11:23:12 +0300

    minor refactoring and logger

--- Learning_to_Route/RL-ECMP/baslines_avg_k.py
+++ Learning_to_Route/RL-ECMP/baslines_avg_k.py
@@ -5,34 +5,43 @@ from ecmp_network import ECMPNetwork, EdgeConsts
 from topologies import topologies
 
 
-def generate_traffic_matrix_baseline(graph: ECMPNetwork,
+def generate_traffic_matrix_baseline(graph: ECMPNetwork, k: int,
                                      matrix_sparsity: float, tm_type, elephant_percentage: float,
                                      network_elephant, network_mice, total_matrices: int):
+    logger.info("Generating baseline of traffic matrices to evaluate of length {}".format(total_matrices + k))
     return [one_sample_tm_base(graph=graph,
                                matrix_sparsity=matrix_sparsity,
                                tm_type=tm_type,
                                elephant_percentage=elephant_percentage, network_elephant=network_elephant,
-                               network_mice=network_mice) for _ in range(total_matrices)]
+                               network_mice=network_mice) for _ in range(total_matrices + k)]
 
 
 def calculate_congestion_per_matrices(net: ECMPNetwork, k: int, traffic_matrix_list: list):
+    logger.info("Calculating congestion to all traffic matrices by {} previous average".format(k))
+
     assert k < len(traffic_matrix_list)
     congestion_list = list()
-    for history_start in range(len(traffic_matrix_list) - k):
-        history_end = history_start + k
-        current_traffic_matrix = traffic_matrix_list[history_end]
-        avg_traffic_matrix = np.mean(traffic_matrix_list[history_start:history_end], axis=0)
+    for index, current_traffic_matrix in enumerate(traffic_matrix_list[k:]):
+
+        logger.debug("Current matrix index is: {}".format(index))
+        avg_traffic_matrix = np.mean(traffic_matrix_list[index:index + k], axis=0)
+
         assert avg_traffic_matrix.shape == current_traffic_matrix.shape
 
+        logger.debug("Solving LP problem for previous {} avenge".format(k))
         _, per_edge_flow_fraction_lp = get_optimal_load_balancing(net, avg_traffic_matrix)  # heuristic flows splittings
 
-        fixes_matrix = np.zeros(avg_traffic_matrix.shape)
-        for i, j in net.get_all_pairs():
-            if avg_traffic_matrix[i][j] == 0 and current_traffic_matrix[i][j] != 0:
-                fixes_matrix[i][j] = current_traffic_matrix[i][j]
+        logger.debug("Handling the flows that exist in real matrix but not in average one")
+        completion_flows_matrix = np.zeros(avg_traffic_matrix.shape)
+        flows_to_check = np.dstack(np.where(current_traffic_matrix != 0))[0]
+        for src, dst in flows_to_check:
+            assert current_traffic_matrix[src][dst] != 0
+            completion_flows_matrix[src][dst] = current_traffic_matrix[src][dst] if avg_traffic_matrix[src][dst] == 0 else 0
 
-        per_edge_flow_fraction_ecmp = get_ecmp_edge_flow_fraction(net,
-                                                                  fixes_matrix)  # for flows in average is Zero but in real are non zero
+        # for flows in average is Zero but in real are non zero
+        per_edge_flow_fraction_ecmp = get_ecmp_edge_flow_fraction(net, completion_flows_matrix)
+
+        logger.debug("Combining all flows fractions")
         per_edge_flow_fraction = dict()
         for edge, frac_matrix in per_edge_flow_fraction_lp.items():
             if edge in per_edge_flow_fraction_ecmp.keys():
@@ -43,6 +52,8 @@ def calculate_congestion_per_matrices(net: ECMPNetwork, k: int, traffic_matrix_l
         for edge, frac_matrix in per_edge_flow_fraction_ecmp.items():
             if edge not in per_edge_flow_fraction.keys():
                 per_edge_flow_fraction[edge] = frac_matrix
+
+        logger.debug('Calculating the congestion per edge and finding max edge congestion')
 
         congestion_per_edge = defaultdict(int)
         max_congestion = 0
@@ -57,12 +68,14 @@ def calculate_congestion_per_matrices(net: ECMPNetwork, k: int, traffic_matrix_l
     return congestion_list
 
 
+K = 1
 ecmpNetwork = ECMPNetwork(topologies["MESH"])
 average_capacity = np.mean(list(ecmpNetwork.get_edges_capacities().values()))
 tms = generate_traffic_matrix_baseline(graph=ecmpNetwork,
+                                       k=K,
                                        matrix_sparsity=0.3,
                                        tm_type=Consts.BIMODAL,
                                        elephant_percentage=0.2, network_elephant=average_capacity, network_mice=average_capacity * 0.1,
-                                       total_matrices=100)
-c_l = calculate_congestion_per_matrices(net=ecmpNetwork, k=3, traffic_matrix_list=tms)
+                                       total_matrices=1000)
+c_l = calculate_congestion_per_matrices(net=ecmpNetwork, k=5, traffic_matrix_list=tms)
 print(np.average(c_l))
