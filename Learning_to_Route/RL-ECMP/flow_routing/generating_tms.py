1e4e1b84fd858fb66a3bcade986dbd4f663a70ba
idoyehe <idoye@campus.technion.ac.il>
2020-05-24 11:53:37 +0300

    adding generating TMs by topology script

---
+++ Learning_to_Route/RL-ECMP/flow_routing/generating_tms.py
@@ -0,0 +1,87 @@
+from Learning_to_Route.data_generation.tm_generation import one_sample_tm_base
+from flow_routing.find_optimal_load_balancing import *
+from logger import logger
+from topologies import topology_zoo_loader
+import pickle
+from consts import TMType
+import os
+from argparse import ArgumentParser
+from sys import argv
+from pathlib import Path
+
+
+def _getOptions(args=argv[1:]):
+    parser = ArgumentParser(description="Parses TMs Generating script arguments")
+    parser.add_argument("-topo", "--topology_url", type=str, help="The url to load graph topology from")
+    parser.add_argument("-cap", "--default_capacity", type=float, help="The capacity for each edge")
+    parser.add_argument("-k", "--k", help="The average k based", type=int)
+    parser.add_argument("-n", "--total_matrices", type=int, help="The number of total matrices")
+    parser.add_argument("-sp", "--sparsity", type=float, help="The sparsity of the matrix")
+    parser.add_argument("-m_type", "--tm_type", type=str, help="The type of the matrix")
+    parser.add_argument("-e_p", "--elephant_percentage", type=float, help="The percentage of elephant flows")
+    parser.add_argument("-n_e", "--network_elephant", type=float, help="The network elephant expectancy")
+    parser.add_argument("-n_m", "--network_mice", type=float, help="The network mice expectancy")
+    options = parser.parse_args(args)
+    return options
+
+
+def _dump_tms_and_opt(net: NetworkClass, url: str, k: int, matrix_sparsity: float, tm_type, elephant_percentage: float,
+                      network_elephant, network_mice, total_matrices: int):
+    tms = _generate_traffic_matrix_baseline(net=net,
+                                            k=k, matrix_sparsity=matrix_sparsity, tm_type=tm_type,
+                                            elephant_percentage=elephant_percentage, network_elephant=network_elephant,
+                                            network_mice=network_mice,
+                                            total_matrices=total_matrices)
+
+    capacity = list(net.get_edges_capacities().values())[0]
+    dict2dump = {"tms": tms, "url": url, "k": k, "capacity": capacity}
+
+    file_name: str = os.getcwd() + "\\TMs_DB\\{}_tms_{}X{}_length_{}_K_{}_{}_sparsity_{}".format(net.get_name, net.get_num_nodes,
+                                                                                                 net.get_num_nodes, total_matrices, k,
+                                                                                                 tm_type, matrix_sparsity)
+    dump_file = open(file_name, 'wb')
+    pickle.dump(dict2dump, dump_file)
+    dump_file.close()
+    return file_name
+
+
+def _generate_traffic_matrix_baseline(net: NetworkClass, k: int,
+                                      matrix_sparsity: float, tm_type, elephant_percentage: float,
+                                      network_elephant, network_mice, total_matrices: int):
+    logger.info("Generating baseline of traffic matrices to evaluate of length {}".format(total_matrices + k))
+    tm_list = list()
+    for _ in range(total_matrices + k):
+        tm = one_sample_tm_base(graph=net,
+                                matrix_sparsity=matrix_sparsity,
+                                tm_type=tm_type,
+                                elephant_percentage=elephant_percentage, network_elephant=network_elephant,
+                                network_mice=network_mice)
+        opt, _ = get_optimal_load_balancing(net, tm)  # heuristic flows splittings
+        tm_list.append((tm, opt))
+
+    return tm_list
+
+
+def load_dump_file(file_name: str):
+    dumped_file = open(Path(file_name), 'rb')
+    dict2load = pickle.load(dumped_file)
+    dumped_file.close()
+    assert isinstance(dict2load, dict)
+    return dict2load
+
+
+if __name__ == "__main__":
+    args = _getOptions()
+    net = NetworkClass(topology_zoo_loader(args.topology_url, default_capacity=args.default_capacity))
+    if args.tm_type == TMType.BIMODAL and args.network_elephant is None:
+        args.network_elephant = 2 * np.mean(list(net.get_edges_capacities().values()))
+        args.network_mice = 0.1 * args.network_elephant
+
+    filename: str = _dump_tms_and_opt(net=net, k=args.k, url=args.topology_url,
+                                      matrix_sparsity=args.sparsity,
+                                      tm_type=args.tm_type,
+                                      elephant_percentage=args.elephant_percentage,
+                                      network_elephant=args.network_elephant,
+                                      network_mice=args.network_mice,
+                                      total_matrices=args.total_matrices)
+    print("Dumps the Tms to:\n{}".format(filename))
