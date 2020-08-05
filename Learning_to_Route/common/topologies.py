a113625022e5dd03ac45008c51d852e194ebdf26
Ido Yehezkel <idoye@campus.technion.ac.il>
2020-08-05 17:25:47 +0300

    represent multigraph as double capacity

--- Learning_to_Route/common/topologies.py
+++ Learning_to_Route/common/topologies.py
@@ -144,19 +144,22 @@ def topology_zoo_loader(url: str, default_capacity: int = SizeConsts.ONE_Gb / Si
         gml = "".join(gml_file.readlines())
         gml_file.close()
 
-    g = nx.parse_gml(gml, label="id")
-    for edge in g.edges:
-        if CAPACITY_LABEL_DEFAULT in g.edges[edge]:
-            g.edges[edge][EdgeConsts.CAPACITY_STR] = int(g.edges[edge][CAPACITY_LABEL_DEFAULT]) / SizeConsts.ONE_Mb
+    raw_g = nx.parse_gml(gml, label="id")
+    parsed_g = nx.Graph()
+    parsed_g.add_nodes_from(raw_g.nodes)
+    for raw_edge in raw_g.edges:
+        edge = (raw_edge[0], raw_edge[1])
+        if edge not in parsed_g.edges:
+            parsed_g.add_edge(u_of_edge=edge[0],v_of_edge=edge[1])
+            parsed_g.edges[edge][EdgeConsts.CAPACITY_STR] = 0
+        if CAPACITY_LABEL_DEFAULT in raw_g.edges[raw_edge]:
+            raw_capacity = int(raw_g.edges[raw_edge][CAPACITY_LABEL_DEFAULT]) / SizeConsts.ONE_Mb
+            parsed_g.edges[edge][EdgeConsts.CAPACITY_STR] += raw_capacity
         else:
-            g.edges[edge][EdgeConsts.CAPACITY_STR] = default_capacity
+            parsed_g.edges[edge][EdgeConsts.CAPACITY_STR] += default_capacity
 
-    g.name = g.graph["Network"]
-    if isinstance(g,nx.MultiGraph):
-        g = nx.MultiGraph(g)
-    else:
-        g = nx.Graph(g)
-    return g
+    parsed_g.graph["Name"] = raw_g.graph["Network"]
+    return parsed_g
 
 
 BASIC_TOPOLOGIES = {
