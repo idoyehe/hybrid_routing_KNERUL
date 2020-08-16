0ef0c359de6a5b216e943ea9615b1d5d4480bbb9
Ido Yehezkel <idoye@campus.technion.ac.il>
2020-08-16 09:48:01 +0300

    update requirements.txt

--- Learning_to_Route/common/rl_env_consts.py
+++ Learning_to_Route/common/rl_env_consts.py
@@ -4,21 +4,23 @@ Created on 6 Feb 2017
 @author: asafvaladarsky
 '''
 
+
 class HistoryConsts:
-    SOFTMIN_ALPHA = -2.0 #tf.constant(-2, dtype=tf.float32)
-    SOFTMAX_ALPHA = 1.0 #tf.constant(-2, dtype=tf.float32)
-    EPSILON = 1.0e-2 #tf.constant(1.0e-6)
+    SOFTMIN_ALPHA = -2.0
+    SOFTMAX_ALPHA = 1.0
+    EPSILON = 1.0e-2
     PERC_DEMAND = 0.999
-    INFTY = 1.0e4 #tf.constant(1.0e4)
-    ZERO = 0.0 #tf.constant(0, dtype=tf.float32)
-    
+    INFTY = 1.0e4
+    ZERO = 0.0
+
     ACTION_SPLITTINT_RATIOS = "splitting"
     ACTION_W_EPSILON = "w_eps"
-    ACTION_W_INFTY = "w_inf" #127 -> 3.27
+    ACTION_W_INFTY = "w_inf"
     ACTION_TM = "tm"
-    
+
     ACTIONS_W = [ACTION_W_EPSILON, ACTION_W_INFTY]
-    
+
+
 class ExtraData:
     REWARD_OVER_FUTURE = "over_future"
     REWARD_OVER_PREV = "over_prev"
@@ -26,7 +28,8 @@ class ExtraData:
     REWARD_OVER_AVG_EXPECTED = "over_avg_expected"
     REWARD_OVER_AVG_ACTUAL = "over_avg_actual"
     REWARD_OVER_RANDOM = "over_random"
-    
+
+
 class WeightStrategy:
     # initial weight assignment strategy
     ONE_OVER_CAPACITY = "one_over"
@@ -42,11 +45,11 @@ class WeightStrategy:
     MIXED_STATIC_THEN_LOCAL = "static_ls"
 
 
-
 class GraphConsts:
     SRC_META_POS = 0
     DST_META_POS = 1
     EDGE_META_POS = 2
+
 
 class RewardType:
     # type of reward functions
@@ -56,20 +59,21 @@ class RewardType:
     REWARD_AVG_UTIL = "avg_util"
 
 
-
 class ActionType:
     ACTION_OBLIVIOUS_PATH = "oblivious"
     ACTION_FLOW_PATH = "with_flows"
+
 
 class ActionIDs:
     INCREASE_ACTION = 0
     DECREASE_ACTION = 1
     NAK_ACTION = 2
 
+
 class GeneralConsts:
-    DEATH_REWARD = -10**4
-    WIN_REWARD = 10**4
-    
+    DEATH_REWARD = -10 ** 4
+    WIN_REWARD = 10 ** 4
+
     SAMPLE_ACTION = 1
 
-    PG_EPOCH = 1#23
+    PG_EPOCH = 1  # 23
