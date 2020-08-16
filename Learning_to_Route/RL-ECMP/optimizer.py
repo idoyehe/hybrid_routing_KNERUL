540e54fddcecb0d292bb40beb96d37a5cf644bca
Ido Yehezkel <idoye@campus.technion.ac.il>
2020-08-16 12:54:32 +0300

    fix optimizer

--- Learning_to_Route/RL-ECMP/optimizer.py
+++ Learning_to_Route/RL-ECMP/optimizer.py
@@ -21,20 +21,22 @@ class WNumpyOptimizer:
         @param edges_capacities: all edges capacities
         @param max_iterations: number of max iterations
         """
-        self._ecmp_network = net
+        self._network = net
         self._max_iterations = max_iterations
         self._nx_graph = True
-        self._graph_adjacency_matrix = self._ecmp_network.get_adjacency
-        self._edges_capacities = self._ecmp_network.get_edges_capacities()
-        self._num_nodes = self._ecmp_network.get_num_nodes
+        self._graph_adjacency_matrix = self._network.get_adjacency
+        self._num_nodes = self._network.get_num_nodes
         self._initialize()
+        self._max_iters = 500
 
     def _initialize(self):
         logger.debug("Building ingoing and outgoing edges map")
-        _, self._ingoing_edges, self._outgoing_edges, self._e_map = self._ecmp_network.build_edges_map()
+        _, self._ingoing_edges, self._outgoing_edges, self._edges_capacities = self._network.build_edges_map()
 
         self._mask = np.ones((self._num_nodes, self._num_nodes), dtype=np.float32) - np.eye(self._num_nodes, dtype=np.float32)
         self._eye_masks = [np.expand_dims(self._mask[:, i], 1) for i in range(self._num_nodes)]
+        self._zero_diagonal = np.ones_like(self._graph_adjacency_matrix, dtype=np.float32) - np.eye(self._num_nodes,
+                                                                                                    dtype=np.float32)
 
     def step(self, weights_vector, traffic_matrix):
         """
@@ -42,42 +44,92 @@ class WNumpyOptimizer:
         :param traffic_matrix: the traffic matrix to examine
         :return: cost and congestion
         """
-        cost, congestion_dict = self._get_cost_given_weights(weights_vector, traffic_matrix)
-        return cost, congestion_dict
+        cost, congestion_per_edge = self._get_cost_given_weights(weights_vector, traffic_matrix)
+        return cost, congestion_per_edge
+
+    def _set_cost_to_dsts(self, weights_vector):
+        tmp = (weights_vector * self._outgoing_edges) @ np.transpose(self._ingoing_edges)
+        tmp[tmp == 0] = HistoryConsts.INFTY
+        return tmp * self._zero_diagonal
+
+    def _bellman_ford(self, w, p_prev, tmp):
+        p_prev = np.reshape(p_prev, [-1])
+        prev_prev = np.zeros_like(p_prev)
+        cur_iter = 0
+        while np.abs(np.sum(p_prev - prev_prev)) > HistoryConsts.EPSILON:
+            tmp_ = p_prev
+            p_prev = np.minimum(p_prev, np.amin(p_prev + tmp, axis=1))
+            prev_prev = tmp_
+            if cur_iter == self._max_iters:
+                break
+            cur_iter += 1
+        return p_prev  # TODO: dst val is infty here
+
+    def _get_s(self, q_val, dst_demand, mask, dst=None):
+        '''
+        input:
+            v: the node with demands
+            demand: the demand of node v
+            q: is the softmax cost vector matrix (dim = |V|x|E|)
+            e_in: is the relationship matrix, a_ie=1 iff e \in In(i) (dim = |V|x|E|)
+        output:
+            s: is the flow input vector (dim=|E|)
+        '''
+
+        # loop magic goes here, basically converts the for loop into matrix operations
+        def get_new_val(prev_iteration, current_demands):
+            current_demand_each_node = self._ingoing_edges @ (np.transpose(q_val * self._outgoing_edges) @ current_demands)
+            return prev_iteration + current_demand_each_node
+
+        prev_iteration = dst_demand
+        current_iteration = get_new_val(dst_demand, dst_demand)
+        cur_iter = 1
+        total_demand = np.sum(dst_demand)
+
+        def does_demand_completed(_prev):
+            return (np.sum(_prev * (1 - mask)) / total_demand) < HistoryConsts.PERC_DEMAND
+
+        while does_demand_completed(current_iteration):
+            res_diff = (current_iteration - prev_iteration) * mask
+            tmp = get_new_val(current_iteration, res_diff)
+            prev_iteration = current_iteration
+            current_iteration = tmp
+            if cur_iter == self._max_iters:
+                break
+            cur_iter += 1
+
+        final_s_value = current_iteration
+        edge_congestion = np.sum(np.transpose(q_val) @ (final_s_value * mask), axis=1)
+        return edge_congestion  # final_s_value,
 
     def _get_cost_given_weights(self, weights_vector, traffic_matrix):
-        logger.debug("Calculate each edge weight")
-        each_edge_weight = (weights_vector * self._outgoing_edges) @ np.transpose(self._ingoing_edges)
-        # calculating each edge it's weights in matrix |edges|x|edges|
-
-        logger.debug("Creating directed graph based on edge weights")
-        directed_graph = nx.from_numpy_matrix(each_edge_weight, create_using=nx.DiGraph())
-        all_shortest_paths_costs = dict(nx.shortest_path_length(directed_graph, weight=EdgeConsts.WEIGHT_STR))
-        # calculating shortest paths form each node to each node, dictionary way
-
         result = np.zeros_like(weights_vector, dtype=np.float32)
-        for dst in range(len(traffic_matrix)):
-            # logger.info("Handle destination: {}".format(dst))
-            dst_demand = np.expand_dims(traffic_matrix[:, dst], 1)  # getting all flows demands to dest from al sources
+        logger.debug("Calculate each edge weight")
 
+        one_hop_cost = (weights_vector * self._outgoing_edges) @ np.transpose(self._ingoing_edges)
+        reduced_directed_graph = nx.from_numpy_matrix(one_hop_cost, create_using=nx.DiGraph())
+        cost_all_adj = dict(nx.shortest_path_length(reduced_directed_graph, weight='weight'))
+
+        for node_dst in self._network.nodes:
+            dst_demand = np.expand_dims(traffic_matrix[:, node_dst], 1)
             total_demands = np.sum(dst_demand)
-
             if total_demands == 0.0:
                 continue
-            # getting all costs to dest from al sources
-            cost_to_dest = [all_shortest_paths_costs[j][dst] for j in range(self._num_nodes)]
-            edge_cost = self.__get_edge_cost(cost_to_dest, each_edge_weight)
-            soft_min_cost_vector = self._soft_min(edge_cost)
 
-            cong = self._get_flow_input_vector(soft_min_cost_vector, dst_demand, total_demands, self._eye_masks[dst], dst)
-            result += np.reshape(cong, [-1])  # , q_val
-        for edge, index in self._e_map.items():
-            congestion = result[index] / self._edges_capacities[edge]
+            cost_adj = [cost_all_adj[i][node_dst] for i in range(self._num_nodes)]
+            edge_cost = self.__get_edge_cost(cost_adj, one_hop_cost)
+            q_val = self._soft_min(edge_cost)
+            cong = self._get_s(q_val, dst_demand, self._eye_masks[node_dst])  # TODO: examine
+            cong = np.reshape(cong, [-1])
+
+            result += cong
+
+        congestion = result / self._edges_capacities
         cost = np.max(congestion)
         return cost, result
 
-    def __get_edge_cost(self, cost_to_dest, each_edge_weight):
-        cost_to_dst1 = cost_to_dest * self._graph_adjacency_matrix + each_edge_weight
+    def __get_edge_cost(self, cost_adj, each_edge_weight):
+        cost_to_dst1 = cost_adj * self._graph_adjacency_matrix + each_edge_weight
         cost_to_dst2 = np.reshape(cost_to_dst1, [-1])
         cost_to_dst3 = cost_to_dst2[cost_to_dst2 != 0]
         return cost_to_dst3 * self._outgoing_edges
@@ -133,18 +185,18 @@ class WNumpyOptimizer:
         edge_congestion = np.sum(np.transpose(softmin_cost_vector) @ (final_s_value * mask), axis=1)
         return edge_congestion  # final_s_value
 
-# from Learning_to_Route.data_generation import tm_generation
-# from topologies import topologies, topology_zoo_loader
-# from Learning_to_Route.common.consts import Consts
-# from flow_routing.find_optimal_load_balancing import get_optimal_load_balancing
-# from flow_routing.find_optimal_load_balancingV2 import get_optimal_load_balancing as get_optimal_load_balancingv2
-#
-# ecmpNetwork = NetworkClass(topologies["TRIANGLE"])
-# tm = tm_generation.one_sample_tm_base(ecmpNetwork, 1, Consts.GRAVITY, 0, 0, 0)
-# # ecmpNetwork = NetworkClass(ecmpNetwork.reducing_undirected2directed()[0])
-#
-# opt = WNumpyOptimizer(ecmpNetwork)
-# print(get_optimal_load_balancing(net=ecmpNetwork, traffic_demands=tm)[0])
-# print(get_optimal_load_balancingv2(net=ecmpNetwork, traffic_demands=tm)[0])
-# cost, congestion_dict = opt.step([1, 0.1, 100, 100, 1, 1], tm)
-# print(cost)
+
+if __name__ == "__main__":
+    from Learning_to_Route.data_generation import tm_generation
+    from common.topologies import BASIC_TOPOLOGIES, topology_zoo_loader
+    from Learning_to_Route.common.consts import TMType
+    from static_routing.optimal_load_balancing import optimal_load_balancing_LP_solver
+
+    ecmpNetwork = NetworkClass(BASIC_TOPOLOGIES["TRIANGLE"])
+    tm = tm_generation.one_sample_tm_base(ecmpNetwork.get_g_directed, 1, TMType.GRAVITY, 0, 0, 0)
+
+    opt = WNumpyOptimizer(ecmpNetwork)
+    opt_congestion, opt_routing_scheme = optimal_load_balancing_LP_solver(net=ecmpNetwork, traffic_matrix=tm)
+    print("Optimal Congestion: {}".format(opt_congestion))
+    cost, congestion_dict = opt.step([1, 0.1, 100, 100, 1, 1], tm)
+    print("Optimizer Congestion: {}".format(cost))
