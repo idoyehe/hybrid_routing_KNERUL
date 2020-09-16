2adcdbda3bef941df13070e19dd70644e73616f3
idoye <idoye@campus.technion.ac.il>
2020-09-16 16:58:30 +0300

    minor refactoring

--- Learning_to_Route/TMs_DB/plot_links_weights.py
+++ Learning_to_Route/TMs_DB/plot_links_weights.py
@@ -15,20 +15,21 @@ def _getOptions(args=argv[1:]):
 
 def load_numpy_object_from_file(file_path: str):
     link_weights_file = open(file_path, "rb")
-    cumulative_list = list()
-
-    while True:
-        try:
-            cumulative_list.append(np.load(link_weights_file))
-        except Exception as e:
-            print("Read from file done, EOF")
-            link_weights_file.close()
-            break
-    assert len(cumulative_list) > 0
-    link_weights_matrix = np.array(cumulative_list).transpose()
-    link_weights_file = open(file_path, "wb")
-    np.save(link_weights_file, link_weights_matrix)
+    link_weights_matrix = np.load(link_weights_file)
     link_weights_file.close()
+    assert len(link_weights_matrix) > 0
+    return link_weights_matrix
+
+
+def plot_link_weights(link_weights_matrix, number_of_links: int, start_time):
+    for link_index in range(number_of_links):
+        fig = plt.figure(num=None, figsize=(12, 10), dpi=80, facecolor='w', edgecolor='k')
+        link_weights_data = link_weights_matrix[link_index][start_time:]
+        plt.title("Link No. {} STD is {}".format(link_index, np.std(link_weights_data)))
+        plt.xlabel("# Timesteps")
+        plt.ylabel("Link Weight")
+        plt.plot(np.arange(start_time, start_time + len(link_weights_data), step=1), link_weights_data)
+        plt.show()
 
 
 if __name__ == "__main__":
@@ -38,3 +39,4 @@ if __name__ == "__main__":
     print("Loading from file path: {}".format(file_path))
     link_weights_matrix = load_numpy_object_from_file(file_path)
     print("Done! loading from file")
+    plot_link_weights(link_weights_matrix, number_of_links, start_time)
