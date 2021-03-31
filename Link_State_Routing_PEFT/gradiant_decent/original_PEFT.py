deb7ef4d6c2c4f44b9d9d709f826d71f6a635872
IdoYe <idoye@campus.technion.ac.il>
2021-03-31 14:32:49 +0300

    print in table format

--- Link_State_Routing_PEFT/gradiant_decent/original_PEFT.py
+++ Link_State_Routing_PEFT/gradiant_decent/original_PEFT.py
@@ -8,6 +8,7 @@ from Link_State_Routing_PEFT.RL.PEFT_optimizer import PEFTOptimizer
 from sys import argv
 from random import shuffle
 import numpy as np
+from tabulate import tabulate
 
 
 def _getOptions(args=argv[1:]):
@@ -18,7 +19,7 @@ def _getOptions(args=argv[1:]):
 
 
 def __initialize_all_weights(net: NetworkClass):
-    return np.ones(shape=(net.get_num_edges), dtype=np.float64)
+    return np.ones(shape=(net.get_num_edges), dtype=np.float64) * 10
 
 
 def __stop_loop(net: NetworkClass, current_flows_values, necessary_capacity_dict):
@@ -27,7 +28,7 @@ def __stop_loop(net: NetworkClass, current_flows_values, necessary_capacity_dict
         edge_idx = net.get_edge2id(u, v)
         delta += np.abs(current_flows_values[edge_idx] - necessary_capacity_dict[u, v])
     print('sum[|necessary_capacity_dict - current_flows_values|] = {}'.format(delta))
-    return delta < 0.3
+    return delta <0.5
 
 
 def __gradient_decent_update(net: NetworkClass, w_u_v, step_size, current_flows_values, necessary_capacity_dict):
@@ -61,40 +62,43 @@ def PEFT_main_loop(net, traffic_matrix, necessary_capacity_dict):
 def experiment(traffic_matrix_list, w_u_v, baseline_objective, r_per_mtrx):
     traffic_distribution = PEFTOptimizer(net, None)
     heursitic_value = 0
-    for idx, (pr, tm, opt) in enumerate(traffic_matrix_list):
-        print("Matrix Number: {}".format(idx))
-        max_congestion, most_congested_link, total_congestion, total_congestion_per_link, total_load_per_link = traffic_distribution.step(
-            w_u_v, tm, None)
-        heursitic_value += pr * max_congestion
-        print("congestion using heuristic link weights is: {}".format(max_congestion))
-        print("Congestion using multiple MCF - LP: {}".format(r_per_mtrx[idx]))
-        print("Congestion using LP optimal: {}".format(opt))
-        print("Ratio, congestion using link weights / Congestion using multiple MCF : {}".format(
-            max_congestion / r_per_mtrx[idx]))
 
-    print("Heuristic objective Vs. expected baseline objective: {}".format(heursitic_value / baseline_objective))
+    data = list()
+    headers = ["# Matrix",
+               "Congestion heuristic link weights",
+               "Congestion using multiple MCF - LP",
+               "Congestion using LP optimal",
+               "Congestion Ratio, link weights / multiple MCF"]
+    for idx, (pr, tm, opt) in enumerate(traffic_matrix_list):
+        max_congestion, _, _, _, _ = traffic_distribution.step(w_u_v, tm, None)
+        heursitic_value += pr * max_congestion
+        data.append([idx, max_congestion, r_per_mtrx[idx], opt, max_congestion / r_per_mtrx[idx]])
+
+    print(tabulate(data, headers=headers))
+    print("Heuristic objective Vs. expected objective baseline: {}".format(heursitic_value / baseline_objective))
     return heursitic_value
 
 
 if __name__ == "__main__":
-    for _ in range(5):
-        options = _getOptions()
-        dumped_path = options.dumped_path
-        loaded_dict = load_dump_file(dumped_path)
-        net = NetworkClass(topology_zoo_loader(loaded_dict["url"], default_capacity=loaded_dict["capacity"]))
-        print("Topology Name: {}".format(net.get_name))
-        shuffle(loaded_dict["tms"])
+    options = _getOptions()
+    dumped_path = options.dumped_path
+    loaded_dict = load_dump_file(dumped_path)
+    net = NetworkClass(topology_zoo_loader(loaded_dict["url"], default_capacity=loaded_dict["capacity"]))
+    print("Topology Name: {}".format(net.get_name))
+    shuffle(loaded_dict["tms"])
 
-        l = 2
-        p = [0.99] + [(1 - 0.99) / (l - 1)] * (l - 1)
-        traffic_matrix_list = [(p[i], t[0]) for i, t in enumerate(loaded_dict["tms"][0:l])]
+    l = 2
+    p = [0.99] + [(1 - 0.99) / (l - 1)] * (l - 1)
+    traffic_matrix_list = [(p[i], t[0]) for i, t in enumerate(loaded_dict["tms"][0:l])]
 
-        baseline_objective, _, r_per_mtrx, _ = multiple_matrices_mcf_LP_baseline_solver(net, traffic_matrix_list)
-        heuristic_optimal, splitting_ratios_per_src_dst_edge, necessary_capacity_dict = multiple_matrices_mcf_LP_heuristic_solver(
-            net, traffic_matrix_list)
+    baseline_objective, _, r_per_mtrx, _ = multiple_matrices_mcf_LP_baseline_solver(net, traffic_matrix_list)
+    heuristic_optimal, splitting_ratios_per_src_dst_edge, necessary_capacity_dict = multiple_matrices_mcf_LP_heuristic_solver(
+        net, traffic_matrix_list)
 
-        expected_tm = sum(pr * t for pr, t in traffic_matrix_list)
+    expected_tm = sum(pr * t for pr, t in traffic_matrix_list)
 
-        w_u_v, PEFT_congestion = PEFT_main_loop(net, expected_tm, necessary_capacity_dict)
+    w_u_v, PEFT_congestion = PEFT_main_loop(net, expected_tm, necessary_capacity_dict)
 
-        experiment(traffic_matrix_list, w_u_v, baseline_objective, r_per_mtrx)
+    traffic_matrix_list = [(p[i], t[0], t[1]) for i, t in enumerate(loaded_dict["tms"][0:l])]
+
+    experiment(traffic_matrix_list, w_u_v, baseline_objective, r_per_mtrx)
--- common/logger.py
+++ common/logger.py
@@ -4,4 +4,4 @@ level = logging.INFO
 logging.basicConfig(format='%(levelname)s:%(message)s', level=level)
 logger = logging.getLogger(__name__)
 logger.setLevel(level)
-logger.disabled = 1
+# logger.disabled = 1
