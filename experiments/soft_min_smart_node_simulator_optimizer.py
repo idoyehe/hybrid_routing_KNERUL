b6f20c31e1f44ca747ba34f4ac8802ba76e91484
idoye <idoye@campus.technion.ac.il>
2021-07-20 10:31:09 +0300

    implement optimizer using LP and flows simulator

--- experiments/soft_min_smart_node_simulator_optimizer.py
+++ experiments/soft_min_smart_node_simulator_optimizer.py
@@ -2,8 +2,8 @@
 Created on 13 May 2021
 @author:: Ido Yehezkel
 """
-
 import numpy as np
+
 from common.RL_Env.rl_env_consts import HistoryConsts
 from common.static_routing.oblivious_routing import calculate_congestion_per_matrices, oblivious_routing
 from common.RL_Env.optimizer_abstract import *
@@ -13,7 +13,6 @@ from common.utils import extract_flows, extract_lp_values
 class SoftMinSmartNodesOptimizer(Optimizer_Abstract):
     def __init__(self, net: NetworkClass, testing=False):
         super(SoftMinSmartNodesOptimizer, self).__init__(net, testing)
-        self._total_load_per_link = None
 
     def step(self, weights_vector, traffic_matrix, optimal_value):
         """
@@ -22,7 +21,7 @@ class SoftMinSmartNodesOptimizer(Optimizer_Abstract):
         :return: cost and congestion
         """
         rl_max_congestion, rl_most_congested_link, rl_total_congestion, \
-        rl_total_congestion_per_link, rl_total_load_per_link = self._get_cost_given_weights(weights_vector, traffic_matrix)
+        rl_total_congestion_per_link, rl_total_load_per_link = self._get_cost_given_weights(weights_vector, traffic_matrix, optimal_value)
 
         return rl_max_congestion, rl_most_congested_link, rl_total_congestion, rl_total_congestion_per_link, rl_total_load_per_link
 
@@ -105,30 +104,11 @@ class SoftMinSmartNodesOptimizer(Optimizer_Abstract):
             splitting_ratios[node_dst] = q_val
         return splitting_ratios
 
-    def _build_src_dst_hybrid_spr(self, weights_vector, flows):
+    def _get_cost_given_weights(self, weights_vector, tm, optimal_value):
         destination_based_spr = self.calculating_destination_based_spr(weights_vector)
-        net_direct = self._network
-        smart_nodes = net_direct.get_smart_nodes
-        smart_nodes_spr = net_direct.get_smart_nodes_spr
-        src_dst_routing_hybrid_spr = dict()
-        for src, dst in flows:
-            _src_dst_routing_hybrid_spr = np.copy(destination_based_spr[dst])
-
-            for u, v in net_direct.edges:
-                u_v_idx = net_direct.get_edge2id(u, v)
-                if u in smart_nodes:
-                    _src_dst_routing_hybrid_spr[u_v_idx] = smart_nodes_spr[src, dst, u_v_idx]
-
-            src_dst_routing_hybrid_spr[src, dst] = _src_dst_routing_hybrid_spr
-
-        return src_dst_routing_hybrid_spr
-
-    def _get_cost_given_weights(self, weights_vector, tm):
-        flows = extract_flows(tm)
-        source_routing_hybrid_spr = self._build_src_dst_hybrid_spr(weights_vector, flows)
 
         rl_max_congestion, rl_most_congested_link, rl_total_congestion, \
-        rl_total_congestion_per_link, rl_total_load_per_link = self._calculating_traffic_distribution(source_routing_hybrid_spr, tm)
+        rl_total_congestion_per_link, rl_total_load_per_link = self._calculating_traffic_distribution(destination_based_spr, tm)
 
         if self._testing:
             logger.info("RL most congested link: {}".format(rl_most_congested_link))
@@ -136,50 +116,117 @@ class SoftMinSmartNodesOptimizer(Optimizer_Abstract):
 
         return rl_max_congestion, rl_most_congested_link, rl_total_congestion, rl_total_congestion_per_link, rl_total_load_per_link
 
-    def _simulate_flow(self, index, source_routing_hybrid_spr, src, dst, demand):
+    def _calculating_traffic_distribution(self, destination_based_spr, tm):
         net_direct = self._network
-        total_load_per_link = np.zeros(shape=(net_direct.get_num_edges), dtype=np.float64)
-        source_routing_hybrid_spr = source_routing_hybrid_spr * self._outgoing_edges
-        online_traffic_vec = np.zeros(shape=(net_direct.get_num_nodes, 1), dtype=np.float64)
-        online_traffic_vec[src] = demand
-        total_traffic_vec = online_traffic_vec
-        completed_demand = 0
-        for _ in range(Consts.MAX_ITER):
-            if completed_demand / demand >= Consts.PREC_DEMAND:
-                break
-            online_traffic_by_edge = online_traffic_vec * source_routing_hybrid_spr
-            total_load_per_link += np.sum(online_traffic_by_edge, axis=0)
-            n2n_online_traffic = online_traffic_by_edge @ self._ingoing_edges.T
-            online_traffic_vec = np.reshape(np.sum(n2n_online_traffic, axis=0), newshape=(net_direct.get_num_nodes, 1))
-            total_traffic_vec += online_traffic_vec
-            completed_demand += online_traffic_vec[dst]
-            online_traffic_vec[dst] = 0
+        smart_nodes = net_direct.get_smart_nodes
+        smart_nodes_spr = net_direct.get_smart_nodes_spr
+
+        self._initialize_gurobi_env()
+        self.gb_env.start()
+
+        mcf_problem = gb.Model(name="LP problem for flows, given network, traffic matrix and splitting_ratios", env=self.gb_env)
+        flows = extract_flows(tm)
+
+        flows_vars_src2dest_per_edge = mcf_problem.addVars(flows, net_direct.edges, name="f", lb=0.0, vtype=GRB.CONTINUOUS)
+        mcf_problem.setObjective(flows_vars_src2dest_per_edge.sum(), sense=GRB.MINIMIZE)
+
+        for src, dst in flows:
+            # Flow conservation at the dst
+            __flow_from_dst = sum(flows_vars_src2dest_per_edge[src, dst, dst, v] for _, v in net_direct.out_edges_by_node(dst))
+            __flow_to_dst = sum(flows_vars_src2dest_per_edge[src, dst, u, dst] for u, _ in net_direct.in_edges_by_node(dst))
+            mcf_problem.addLConstr(__flow_to_dst, GRB.EQUAL, tm[src, dst])
+            mcf_problem.addLConstr(__flow_from_dst, GRB.EQUAL, 0.0)
+
+            for u in net_direct.nodes:
+                if u == dst:
+                    continue
+                # Flow conservation at src / transit node
+                __flow_from_u = sum(flows_vars_src2dest_per_edge[src, dst, u, v] for _, v in net_direct.out_edges_by_node(u))
+                __flow_to_u = sum(flows_vars_src2dest_per_edge[src, dst, v, u] for v, _ in net_direct.in_edges_by_node(u))
+                if u == src:
+                    mcf_problem.addLConstr(__flow_from_u, GRB.EQUAL, __flow_to_u + tm[src, dst])
+                else:
+                    mcf_problem.addLConstr(__flow_from_u, GRB.EQUAL, __flow_to_u)
+
+                for _u, v in net_direct.out_edges_by_node(u):
+                    assert u == _u
+                    del _u
+                    u_v_idx = net_direct.get_edge2id(u, v)
+
+                    spr = destination_based_spr[dst, u_v_idx]  # default assignments
+                    if u in smart_nodes:
+                        src_dst_spr = smart_nodes_spr[src, dst, u_v_idx]
+                        spr = src_dst_spr if not np.isnan(src_dst_spr) else spr
+
+                    mcf_problem.addLConstr(__flow_from_u * spr, GRB.EQUAL, flows_vars_src2dest_per_edge[src, dst, u, v])
+
+            mcf_problem.update()
+
+        try:
+            logger.debug("LP Submit to Solve {}".format(mcf_problem.ModelName))
+            mcf_problem.optimize()
+            assert mcf_problem.Status == GRB.OPTIMAL
+        except AssertionError as e:
+            logger_level = logger.level
+            logger.setLevel(logging.DEBUG)
+            if mcf_problem.Status == GRB.UNBOUNDED:
+                logger.debug('The model cannot be solved because it is unbounded')
+                raise Exception("****Optimize failed****\nStatus is NOT optimal but {}".format(mcf_problem.Status))
+
+            if mcf_problem.Status != GRB.INF_OR_UNBD and mcf_problem.Status != GRB.INFEASIBLE:
+                logger.debug('Optimization was stopped with status {}'.format(mcf_problem.Status))
+                raise Exception("****Optimize failed****\nStatus is NOT optimal but {}".format(mcf_problem.Status))
+
+            orignumvars = mcf_problem.NumVars
+            mcf_problem.feasRelaxS(0, False, False, True)
+            mcf_problem.optimize()
+
+            if mcf_problem.Status != GRB.OPTIMAL:
+                tm_file_name = "C:\\Users\\IdoYe\\PycharmProjects\\Research_Implementing\\tm.npy"
+                tm_file = open(tm_file_name, 'wb')
+                np.save(tm_file, tm)
+                tm_file.close()
+
+                sr_file_name = "C:\\Users\\IdoYe\\PycharmProjects\\Research_Implementing\\sr.npy"
+                sr_file = open(sr_file_name, 'wb')
+                np.save(sr_file, destination_based_spr)
+                sr_file.close()
+                logger.info('Model is infeasible {}'.format(mcf_problem.Status))
+                raise Exception("****Optimize failed****\nStatus is NOT optimal but {}".format(mcf_problem.Status))
+
+            assert logger.level == logging.DEBUG
+            slacks = mcf_problem.getVars()[orignumvars:]
+            for sv in slacks:
+                if sv.X > Consts.FEASIBILITY_TOL:
+                    logger.debug('Slack value:{} = {}'.format(sv.VarName, sv.X))
+
+            logger.setLevel(logger_level)
+
+        except gb.GurobiError as e:
+            raise Exception("****Optimize failed****\nException is:\n{}".format(e))
 
         if logger.level == logging.DEBUG:
-            for u in net_direct.nodes:
-                input_traffic = 0
-                if u == src:
-                    input_traffic += demand
-                for v, _ in net_direct.in_edges_by_node(u):
-                    if v == dst:
-                        continue
-                    v_u_idx = net_direct.get_edge2id(v, u)
-                    input_traffic += float(total_traffic_vec[v]) * source_routing_hybrid_spr[v][v_u_idx]
-                if error_bound(np.abs(input_traffic / total_traffic_vec[u]), 1e-2):
-                    logger.debug("mismatch in simulation flow ({},{}) in node {}".format(src, dst, u))
-        self._total_load_per_link[index] = total_load_per_link
+            mcf_problem.printStats()
+            mcf_problem.printQuality()
 
-    def _calculating_traffic_distribution(self, source_routing_hybrid_spr, tm):
-        net_direct = self._network
-        flows = extract_flows(tm)
-        self._total_load_per_link = np.zeros(shape=(len(flows), net_direct.get_num_edges), dtype=np.float64)
-        for index, (src, dst) in enumerate(flows):
-            self._simulate_flow(index, source_routing_hybrid_spr[src, dst], src, dst, tm[src, dst])
+        flows_vars_src2dest_per_edge = extract_lp_values(flows_vars_src2dest_per_edge)
+        mcf_problem.close()
+        self.gb_env.close()
 
-        total_load_per_link = np.sum(self._total_load_per_link, axis=0)
+        self.__validate_flow(net_direct, tm, flows_vars_src2dest_per_edge, (destination_based_spr, smart_nodes_spr))
+
+        flows_vars_per_edge_dict = dict()
+        total_load_per_link = np.zeros((net_direct.get_num_edges), dtype=np.float64)
+
+        for u, v in net_direct.edges:
+            flows_vars_per_edge_dict[(u, v)] = sum(flows_vars_src2dest_per_edge[(src, dst, u, v)] for src, dst in flows)
+            edge_index = net_direct.get_edge2id(u, v)
+            total_load_per_link[edge_index] = flows_vars_per_edge_dict[(u, v)]
+
         total_congestion_per_link = total_load_per_link / self._edges_capacities
 
         most_congested_link = np.argmax(total_congestion_per_link)
         max_congestion = total_congestion_per_link[most_congested_link]
         total_congestion = np.sum(total_congestion_per_link)
+
         return max_congestion, most_congested_link, total_congestion, total_congestion_per_link, total_load_per_link
