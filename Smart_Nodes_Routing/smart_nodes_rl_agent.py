829ff35c6d80d29d1776fa3a82024d653ed59c48
IdoYe <idoye@campus.technion.ac.il>
2021-10-17 10:21:47 +0300

    minor changes

--- Smart_Nodes_Routing/smart_nodes_rl_agent.py
+++ Smart_Nodes_Routing/smart_nodes_rl_agent.py
@@ -12,6 +12,7 @@ def _getOptions(args=argv[1:]):
     parser.add_argument("-l_agent", "--load_agent", type=str, help="Load a dumped agent", default=None)
     parser.add_argument("-l_net", "--load_network", type=str, help="Load a dumped Network object", default=None)
     parser.add_argument("-n_iter", "--number_of_iterations", type=int, help="Number of iteration", default=2)
+    parser.add_argument("-policy_updates", "--policy_updates", type=int, help="Number of policy updates", default=200)
     options = parser.parse_args(args)
     return options
 
@@ -24,12 +25,13 @@ if __name__ == "__main__":
     load_agent = args.load_agent
     load_network = args.load_network
     num_of_iterations = args.number_of_iterations
+    policy_updates = args.policy_updates
 
     if load_agent is None:
         logger.info("********* Iteration 0 Starts *********")
-        model, single_env = model_learn(config_folder, "Iteration_0", load_agent, load_network, policy_updates=1000)
+        model, single_env = model_learn(config_folder, "Iteration_0", load_agent, load_network)
     else:
-        model, single_env = model_learn(config_folder, "Iteration_0", load_agent, load_network, policy_updates=200)
+        model, single_env = model_learn(config_folder, "Iteration_0", load_agent, load_network, policy_updates=policy_updates)
 
         logger.info("***************** Iteration 0 Finished ******************")
 
@@ -40,7 +42,8 @@ if __name__ == "__main__":
         destination_based_sprs = single_env.get_optimizer.calculating_destination_based_spr(link_weights)
         env_train_observations = single_env.get_train_observations
 
-        best_smart_nodes = greedy_best_smart_nodes_and_spr(single_env.get_network, env_train_observations[0], destination_based_sprs, number_smart_nodes, smart_nodes_set)
+        best_smart_nodes = greedy_best_smart_nodes_and_spr(single_env.get_network, env_train_observations[0], destination_based_sprs,
+                                                           number_smart_nodes, smart_nodes_set)
 
         current_smart_nodes = best_smart_nodes[0]
         single_env.set_network_smart_nodes_and_spr(current_smart_nodes, best_smart_nodes[2])
