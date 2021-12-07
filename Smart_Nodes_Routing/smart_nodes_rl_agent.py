aa436cc859909d8ef34074388c3bd7ad6d71b93e
idoye <idoye@campus.technion.ac.il>
2021-12-07 16:45:39 +0200

    new title dynamic creation

--- Smart_Nodes_Routing/smart_nodes_rl_agent.py
+++ Smart_Nodes_Routing/smart_nodes_rl_agent.py
@@ -1,11 +1,13 @@
 import sys
+
 sys.path.append("/home/idoye/PycharmProjects/Research_Implementing")
 
 from common.logger import logger
-from rl_env.model_functions import model_learn, greedy_best_smart_nodes_and_spr, model_continue_learning, run_testing,get_json_file_from_cfg
+from rl_env.model_functions import model_learn, greedy_best_smart_nodes_and_spr, model_continue_learning, run_testing, get_json_file_from_cfg
 from argparse import ArgumentParser
 from sys import argv
 from common.utils import load_dump_file
+
 
 def _getOptions(args=argv[1:]):
     parser = ArgumentParser(description="Parses TMs Generating script arguments")
@@ -43,6 +45,8 @@ if __name__ == "__main__":
 
         logger.info("***************** Iteration 0 Finished ******************")
 
+    net_title = single_env.get_network.get_title
+    lp_train_len = len(lp_tms)
     current_smart_nodes = tuple()
     for i in range(1, num_of_iterations + 1):
         logger.info("***** Iteration {}, Evaluating Smart Node  *****".format(i))
@@ -59,8 +63,7 @@ if __name__ == "__main__":
 
         logger.info("********** Iteration {}, Smart Nodes:{}  ***********".format(i, current_smart_nodes))
         logger.info("********* Iteration {} Starts, Agent is learning *********".format(i))
-        # learning_title = "Iteration_{}".format(i)
-        learning_title = "Claranet_agent_baseline_4_smart_nodes_4096"
+        learning_title = "{}_agent_baseline_{}_smart_nodes_{}".format(net_title, len(current_smart_nodes), lp_train_len)
         model, single_env = model_continue_learning(model, single_env, learning_title, policy_updates=policy_updates)
 
     logger.info("========================== Learning Process is Done =================================")
