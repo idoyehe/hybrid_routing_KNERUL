9d42b7ca6097613c6c9c5a268df3c47b2f352768
Ido Yehezkel <idoye@campus.technion.ac.il>
2022-05-30 17:00:58 +0300

    plots

--- Smart_Nodes_Routing/plots/graphs_plot.py
+++ Smart_Nodes_Routing/plots/graphs_plot.py
@@ -20,16 +20,17 @@ def _getOptions(args=argv[1:]):
     parser = ArgumentParser(description="Parses TMs Generating script arguments")
     parser.add_argument("-topo", "--topology_name", type=str, help="The topology name")
     parser.add_argument("-traffic", "--traffic_name", type=str, help="The traffic distribution name")
-    parser.add_argument("-obliv_base", "--oblivious_baseline", type=eval, help="If oblivious is the baseline", default=False)
+    parser.add_argument("-obliv_base", "--oblivious_baseline", type=eval, help="If oblivious is the baseline",
+                        default=False)
     options = parser.parse_args(args)
     return options
 
 
 def plot_baselines_graphs(save_file, x_labels, y_data, oblivious_baseline, h_lines=None):
     bar_design = dict()
-    bar_design["Non-Key Nodes Train Set"] = {"color": "mediumseagreen", "hatch": '/',"alpha":0.3}
-    bar_design["Key Nodes Train Set"] = {"color": "seagreen", "hatch": 'o',"alpha":0.3}
-    bar_design["Test Sets"] = {"color": "forestgreen", "fill": True,"hatch":None,"alpha":1.0}
+    bar_design["Non-Key Nodes Train Set"] = {"color": "darkseagreen", "hatch": '.', "alpha": 0.8}
+    bar_design["Key Nodes Train Set"] = {"color": "palegreen", "hatch": 'x', "alpha": 0.8}
+    bar_design["Test Sets"] = {"color": "darkgreen", "fill": True, "hatch": None, "alpha": 1.0}
 
     fontsize = 11
     fig = plt.figure(figsize=(7.3, 5))
@@ -45,8 +46,11 @@ def plot_baselines_graphs(save_file, x_labels, y_data, oblivious_baseline, h_lin
     for y_label, value in y_data.items():
         y_data, yerr = tuple(zip(*value))
         ax.bar(ind + (offset * offset_value), y_data, width=width, align='center', label=y_label, yerr=yerr,
-               error_kw=dict(lw=0.8, capsize=1.8, capthick=0.8, ecolor='black'), color=bar_design[y_label]["color"],hatch=bar_design[y_label]["hatch"],
-               alpha=bar_design[y_label]["alpha"])
+               error_kw=dict(lw=0.8, capsize=1.8, capthick=0.8, ecolor='black'),
+               color=bar_design[y_label]["color"],
+               hatch=bar_design[y_label]["hatch"],
+               alpha=bar_design[y_label]["alpha"]
+               )
         offset += 1
         y_tick_offset = min(y_tick_offset, y_data[-2] - y_data[-1])
         y_max = max(y_max, max(y_data))
@@ -62,9 +66,8 @@ def plot_baselines_graphs(save_file, x_labels, y_data, oblivious_baseline, h_lin
     ax.yaxis.grid()  # grid lines
     ax.set_axisbelow(True)  # grid lines are behind the rest
     bottom = 0.0 if oblivious_baseline else 1.0
-    plt.title("Random Scale Free 30 Nodes")
     plt.ylim(bottom=bottom)
-    plt.yticks(np.arange(bottom, 1.34, step=y_tick_offset *5), fontsize=fontsize)
+    plt.yticks(np.arange(bottom, 2, step=y_tick_offset * 3), fontsize=fontsize)
     ax.yaxis.set_major_formatter(mtick.FormatStrFormatter('%.2f'))
     plt.tight_layout()
     plt.savefig(save_file)
@@ -72,6 +75,10 @@ def plot_baselines_graphs(save_file, x_labels, y_data, oblivious_baseline, h_lin
 
 
 def plot_cross_topologies_graphs(save_file, topologies, y_data):
+    bar_design = dict()
+    bar_design["Uniform Link Weight\nInitialized RL"] = {"color": "darkseagreen", "hatch": '.', "alpha": 0.8}
+    bar_design["Optimized Link Weight\nInitialization"] = {"color": "palegreen", "hatch": 'x', "alpha": 0.8}
+    bar_design["Optimized Link Weight\nInitialized RL"] = {"color": "darkgreen", "fill": True, "hatch": None, "alpha": 1.0}
     fontsize = 11
     fig = plt.figure(figsize=(7.3, 5))
     ax = plt.subplot()
@@ -85,7 +92,10 @@ def plot_cross_topologies_graphs(save_file, topologies, y_data):
     y_max = 0
     for y_label, value in y_data.items():
         y_data, yerr = tuple(zip(*value))
-        ax.bar(ind + (offset * offset_value), y_data, width=width, align='center', label=y_label, yerr=yerr, ecolor='black', capsize=5)
+        ax.bar(ind + (offset * offset_value), y_data, width=width, align='center', label=y_label,yerr=yerr,
+               error_kw=dict(ecolor='black', capsize=5),
+               color=bar_design[y_label]["color"], hatch=bar_design[y_label]["hatch"],
+               alpha=bar_design[y_label]["alpha"])
         offset += 1
         y_tick_offset = min(y_tick_offset, np.abs(y_data[-2] - y_data[-1]))
         y_max = max(y_max, max(y_data))
@@ -98,7 +108,7 @@ def plot_cross_topologies_graphs(save_file, topologies, y_data):
     ax.yaxis.grid()  # grid lines
     ax.set_axisbelow(True)  # grid lines are behind the rest
     plt.ylim(bottom=1.0)
-    plt.yticks(np.arange(1.0, 1.32, step=y_tick_offset * 0.15), fontsize=fontsize)
+    plt.yticks(np.arange(1.0, 1.34, step=y_tick_offset *0.15), fontsize=fontsize)
     ax.yaxis.set_major_formatter(mtick.FormatStrFormatter('%.2f'))
     plt.tight_layout()
     plt.savefig(save_file)
@@ -113,14 +123,14 @@ if __name__ == "__main__":
 
     baseline_name = "oblivious" if oblivious_baseline else "optimal"
 
-    # save_file = "{}_{}_baseline_{}.pgf".format(topology_name, traffic_name, baseline_name)
-    save_file = "{}_{}_baseline_{}.png".format(topology_name, traffic_name, baseline_name)
+    save_file = "{}_{}_baseline_{}.pdf".format(topology_name, traffic_name, baseline_name)
+    # save_file = "{}_{}_baseline_{}.png".format(topology_name, traffic_name, baseline_name)
 
     x_labels, y_data, h_lines = parsing_data_results(topology_name, traffic_name, oblivious_baseline)
     plot_baselines_graphs(save_file, x_labels, y_data, oblivious_baseline, h_lines)
 
-    # # save_file = "{}_{}.pgf".format("link_weights_init", traffic_name)
+    save_file = "{}_{}.pgf".format("link_weights_init", traffic_name)
     # save_file = "{}_{}.png".format("link_weights_init", traffic_name)
-    #
+
     # topologies, y_data = parse_rl_optimization_cross_topologies(traffic_name)
     # plot_cross_topologies_graphs(save_file, topologies, y_data)
