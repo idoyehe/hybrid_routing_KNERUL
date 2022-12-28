e488793f1b2ba922f1b71fd035c113c6e500769a
idoye <idoye@campus.technion.ac.il>
2022-12-28 08:17:31 +0200

    plotting COPE results update

--- Smart_Nodes_Routing/plots/parsing_data_results.py
+++ Smart_Nodes_Routing/plots/parsing_data_results.py
@@ -9,6 +9,7 @@ JSON_PATH = os.path.join(DIR_NAME, "../experiments/Evaluations_V3.json")
 def __parse_training_set(data_training_set, obliv_base, x_labels):
     baseline = data_training_set["oblivious_routing"] if obliv_base else data_training_set["optimal"]
     oblivious_routing_ratio = (data_training_set["oblivious_routing"] / baseline - 1) * 100
+    cope_routing_ratio = (data_training_set["cope_routing"] / baseline - 1) * 100
     averaged_tm_optimal_routing_ratio = (data_training_set["averaged_tm_optimal_routing_scheme"] / baseline - 1) * 100
     max_tm_optimal_routing_ratio = (data_training_set["max_tm_optimal_routing_scheme"] / baseline - 1) * 100
     weight_initialization_ratio = (data_training_set["smart_weight_initialization"] / baseline - 1) * 100
@@ -19,12 +20,13 @@ def __parse_training_set(data_training_set, obliv_base, x_labels):
         y_data.append((np.mean(label_ratios), np.std(label_ratios)))
     # y_data.insert(1, (weight_initialization_ratio, 0))
 
-    return y_data, oblivious_routing_ratio, averaged_tm_optimal_routing_ratio, max_tm_optimal_routing_ratio
+    return y_data, oblivious_routing_ratio, averaged_tm_optimal_routing_ratio, max_tm_optimal_routing_ratio, cope_routing_ratio
 
 
 def __parse_testing_set(data_testing_set, obliv_base, x_labels, weight_init=False):
     baselines = data_testing_set["oblivious_routing"] if obliv_base else data_testing_set["optimal"]
     oblivious_routing_ratios = [(s / baselines[i] - 1) * 100 for i, s in enumerate(data_testing_set["oblivious_routing"])]
+    cope_routing_ratios = [(s / baselines[i] - 1) * 100 for i, s in enumerate(data_testing_set["cope_routing"])]
     averaged_tm_optimal_routing_ratios = [(s / baselines[i] - 1) * 100 for i, s in enumerate(data_testing_set["averaged_tm_optimal_routing_scheme"])]
     max_tm_optimal_routing_ratios = [(s / baselines[i] - 1) * 100 for i, s in enumerate(data_testing_set["max_tm_optimal_routing_scheme"])]
     if weight_init:
@@ -38,7 +40,7 @@ def __parse_testing_set(data_testing_set, obliv_base, x_labels, weight_init=Fals
     if weight_init:
         y_data.insert(1, (np.mean(weight_initialization_ratios), np.std(weight_initialization_ratios)))
 
-    return y_data, oblivious_routing_ratios, averaged_tm_optimal_routing_ratios, max_tm_optimal_routing_ratios
+    return y_data, oblivious_routing_ratios, averaged_tm_optimal_routing_ratios, max_tm_optimal_routing_ratios, cope_routing_ratios
 
 
 def parse_rl_optimization_cross_topologies(traffic):
@@ -57,7 +59,7 @@ def parse_rl_optimization_cross_topologies(traffic):
     for i, label in enumerate(x_labels):
         y_data[label] = []
         for topo in topologies_raw_names:
-            _y_data, _, _, _ = __parse_testing_set(data[topo][traffic]["results"]["testing_sets"], False, raw_labels, True)
+            _y_data, _, _, _, _ = __parse_testing_set(data[topo][traffic]["results"]["testing_sets"], False, raw_labels, True)
             y_data[label].append(_y_data[i])
 
     return topologies, y_data
@@ -74,27 +76,29 @@ def parsing_data_results(topology_name, traffic, obliv_base):
                     "5_key_node") if "5_key_node" in data_topo_traffic["RL_training_set"].keys() else (
         "naive_RL", "first_RL_phase", "1_key_node", "2_key_node", "3_key_node", "4_key_node")
 
-    y_data_rl_train, oblivious_routing_ratio_rl_train, averaged_tm_optimal_routing_ratio_rl_train, max_tmoptimal_routing_ratio_rl_train = __parse_training_set(
+    y_data_rl_train, oblivious_routing_ratio_rl_train, averaged_tm_optimal_routing_ratio_rl_train, max_tmoptimal_routing_ratio_rl_train, cope_routing_ratios_rl_train = __parse_training_set(
         data_topo_traffic["RL_training_set"], obliv_base, x_raw_labels)
 
-    y_data_lp_train, oblivious_routing_ratio_lp_train, averaged_tm_optimal_routing_ratio_lp_train, max_tm_optimal_routing_ratio_lp_train = __parse_training_set(
+    y_data_lp_train, oblivious_routing_ratio_lp_train, averaged_tm_optimal_routing_ratio_lp_train, max_tm_optimal_routing_ratio_lp_train, cope_routing_ratios_lp_train = __parse_training_set(
         data_topo_traffic["LP_training_set"], obliv_base, x_raw_labels)
 
-    y_data_test, oblivious_routing_ratios_test, averaged_tm_optimal_routing_ratios_lp_test, max_tm_optimal_routing_ratios_lp_test = __parse_testing_set(
+    y_data_test, oblivious_routing_ratios_test, averaged_tm_optimal_routing_ratios_test, max_tm_optimal_routing_ratios_test, cope_routing_ratios_test = __parse_testing_set(
         data_topo_traffic["testing_sets"], obliv_base, x_raw_labels)
 
     oblivious_routing_ratios = [oblivious_routing_ratio_rl_train, oblivious_routing_ratio_lp_train] + oblivious_routing_ratios_test
+    cope_routing_ratios = [cope_routing_ratios_rl_train, cope_routing_ratios_lp_train] + cope_routing_ratios_test
+
     averaged_tm_optimal_routing_ratios = [averaged_tm_optimal_routing_ratio_rl_train,
-                                          averaged_tm_optimal_routing_ratio_lp_train] + averaged_tm_optimal_routing_ratios_lp_test
+                                          averaged_tm_optimal_routing_ratio_lp_train] + averaged_tm_optimal_routing_ratios_test
 
     max_tm_optimal_routing_ratios = [max_tmoptimal_routing_ratio_rl_train,
-                                     max_tm_optimal_routing_ratio_lp_train] + max_tm_optimal_routing_ratios_lp_test
+                                     max_tm_optimal_routing_ratio_lp_train] + max_tm_optimal_routing_ratios_test
 
     if obliv_base:
         h_lines = [("Averaged TM Optimal Routing", "black", "dotted", np.mean(averaged_tm_optimal_routing_ratios)),
                    ("Max TM Optimal Routing", "red", "dashed", np.mean(max_tm_optimal_routing_ratios))]
     else:
-        h_lines = [("Oblivious Routing", "blue", "solid", np.mean(oblivious_routing_ratios)),
+        h_lines = [("Oblivious Routing", "blue", "solid", np.mean(oblivious_routing_ratios)), ("Cope Routing", "orange", "solid", np.mean(cope_routing_ratios)),
                    ("Averaged TM Optimal Routing", "black", "dotted", np.mean(averaged_tm_optimal_routing_ratios)),
                    ("Max TM Optimal Routing", "red", "dashed", np.mean(max_tm_optimal_routing_ratios))]
 
